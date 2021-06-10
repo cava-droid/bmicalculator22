663588c1a1214fd323f56f87af3f1c0d504ef00e
cava <cava@kaist.ac.kr>
2021-06-10 15:37:25 +0900

    startActivity 해결 binding.root 부모 Activity 지정

--- app/src/main/java/com/asknsolve/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/asknsolve/bmicalculator/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.asknsolve.bmicalculator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.asknsolve.bmicalculator.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.resultButton.setOnClickListener {
+            val intent = Intent(this,ResultActivity::class.java)
+        //    Toast.makeText(applicationContext,"버튼이 눌렸어요.",Toast.LENGTH_LONG).show()
+            startActivity(intent)
+        }
     }
 }
