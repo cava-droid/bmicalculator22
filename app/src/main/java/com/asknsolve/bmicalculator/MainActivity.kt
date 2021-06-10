ddc597eead7778dc3147a560b71e80858cebfce8
cava <cava@kaist.ac.kr>
2021-06-10 15:56:46 +0900

    bmi 계산결과 완료

--- app/src/main/java/com/asknsolve/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/asknsolve/bmicalculator/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.asknsolve.bmicalculator.databinding.ActivityMainBinding
+import org.jetbrains.anko.startActivity
 
 private lateinit var binding: ActivityMainBinding
 
@@ -15,9 +16,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.resultButton.setOnClickListener {
-            val intent = Intent(this,ResultActivity::class.java)
+        /*    val intent = Intent(this,ResultActivity::class.java)
         //    Toast.makeText(applicationContext,"버튼이 눌렸어요.",Toast.LENGTH_LONG).show()
-            startActivity(intent)
+            startActivity(intent)*/
+
+            //Anko 라이브러리 사용
+            startActivity<ResultActivity>(
+                "weight" to binding.weightEditText.text.toString(),
+                "height" to binding.heightEditText.text.toString()
+            )
+
+/*          intent.putExtra("weight",binding.weightEditText.text.toString())
+            intent.putExtra("height",binding.heightEditText.text.toString())
+            startActivity(intent)*/
         }
     }
 }
--- app/src/main/java/com/asknsolve/bmicalculator/ResultActivity.kt
+++ app/src/main/java/com/asknsolve/bmicalculator/ResultActivity.kt
@@ -1,11 +1,50 @@
 package com.asknsolve.bmicalculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.asknsolve.bmicalculator.databinding.ActivityResultBinding
+
+private lateinit var binding: ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result)
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //전달받은 키와 몸무게
+        val height = intent.getStringExtra("height")!!.toInt()
+        val weight = intent.getStringExtra("weight")!!.toInt()
+
+        //BMI 계산
+        val bmi = weight / Math.pow(height / 100.0, 2.0)
+
+        //결과 표시
+        when{
+            bmi >= 35 -> binding.resultTextView.text = "고도 비만"
+            bmi >= 30 -> binding.resultTextView.text = "2단계 비만"
+            bmi >= 25 -> binding.resultTextView.text = "1단계 비만"
+            bmi >= 23 -> binding.resultTextView.text = "과체중"
+            bmi >= 18.5 -> binding.resultTextView.text = "정상"
+            else -> binding.resultTextView.text = "저체중"
+        }
+
+        //이미지 표시
+        when{
+            bmi >= 23 ->
+                binding.imageView2.setImageResource(
+                    R.drawable.ic_baseline_sentiment_very_dissatisfied_24
+                )
+
+            bmi >= 18.5 ->
+                binding.imageView2.setImageResource(
+                    R.drawable.ic_baseline_sentiment_satisfied_alt_24
+                )
+
+            else ->
+                binding.imageView2.setImageResource(
+                    R.drawable.ic_baseline_sentiment_dissatisfied_24
+                )
+        }
     }
 }
